6f05a838f3fa7dfd0aae47fe1ad579e8dce91427
Dimo Markov <dimomarkov@gmail.com>
2022-08-29 18:43:06 +0300

    Code cleanup; Catch2 exception stringifier added

--- test/Main.hpp
+++ test/Main.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include <iostream>
 #include <Langulus.Flow.hpp>
 using namespace Langulus;
 using namespace Langulus::Flow;
 
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
+
+namespace std
+{
+   inline ostream& operator << (ostream& os, const ::Langulus::Exception& value) {
+      const ::Langulus::Anyness::Text stringified {value};
+      os << Token {stringified};
+      return os;
+   }
+}

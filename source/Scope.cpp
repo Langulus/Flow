ea93d67c8b39a098cc8c19087a460fe470f8ace2
Dimo Markov <dimomarkov@gmail.com>
2022-10-30 15:09:54 +0200

    Now buildable as static/shared/object library; Warning fixes for GCC

--- source/Scope.cpp
+++ source/Scope.cpp
@@ -313,10 +313,12 @@ namespace Langulus::Flow
          ++executed;
       }
 
-      if (executed)
+      if (executed) {
          VERBOSE(Logger::Green << "OR scope done: " << *this);
-      else
+      }
+      else {
          VERBOSE(Logger::Red << "OR scope failed: " << *this);
+      }
       return executed;
    }
 

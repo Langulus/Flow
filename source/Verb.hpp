5ec5482581a83b913eb2baa8aad3a2fc0770d427
Dimo Markov <dimomarkov@gmail.com>
2024-02-20 13:12:09 +0200

    Moved A::Verb and VerbState to Anyness

--- source/Verb.hpp
+++ source/Verb.hpp
@@ -9,73 +9,8 @@
 #pragma once
 #include "Common.hpp"
 #include "Code.hpp"
-#include "VerbState.hpp"
+#include <Anyness/Verb.hpp>
 
-
-namespace Langulus::A
-{
-
-   ///                                                                        
-   /// Abstract verb, dictating canonical verb size, used in various concepts 
-   ///                                                                        
-   struct Verb : Anyness::Any, Flow::Charge {
-      LANGULUS(NAME) "AVerb";
-      LANGULUS(POD) false;
-      LANGULUS(NULLIFIABLE) false;
-      LANGULUS(DEEP) false;
-      LANGULUS_BASES(Any, Charge);
-
-   protected:
-      // Verb meta, mass, rate, time and priority                       
-      mutable VMeta mVerb {};
-      // The number of successful executions                            
-      Count mSuccesses {};
-      // Verb short-circuiting                                          
-      Flow::VerbState mState {};
-      // Verb context                                                   
-      Any mSource;
-      // The container where output goes after execution                
-      Any mOutput;
-
-      LANGULUS_MEMBERS(
-         &Verb::mVerb,
-         &Verb::mState,
-         &Verb::mSource
-      );
-   };
-
-} // namespace Langulus::A
-
-namespace Langulus::CT
-{
-   
-   /// A VerbBased type is any type that inherits A::Verb                     
-   template<class...T>
-   concept VerbBased = (DerivedFrom<T, A::Verb> and ...);
-
-   /// A reflected verb type is any type that inherits A::Verb, is binary     
-   /// compatible to it, and is reflected as a verb                           
-   template<class...T>
-   concept Verb = VerbBased<T...> and ((
-         sizeof(T) == sizeof(A::Verb) and (
-            requires {
-               {Decay<T>::CTTI_Verb} -> Similar<Token>;
-            } or requires {
-               {Decay<T>::CTTI_PositiveVerb} -> Similar<Token>;
-               {Decay<T>::CTTI_NegativeVerb} -> Similar<Token>;
-            }
-      )) and ...);
-
-   /// Concept for recognizing arguments, with which a verb can be constructed
-   template<class T1, class...TN>
-   concept VerbMakable = Inner::UnfoldInsertable<T1, TN...>
-        or (sizeof...(TN) == 0 and VerbBased<Desem<T1>>);
-
-   /// Concept for recognizing argument, with which a verb can be assigned    
-   template<class A>
-   concept VerbAssignable = VerbMakable<A>;
-
-} // namespace Langulus::CT
 
 namespace Langulus::Flow
 {

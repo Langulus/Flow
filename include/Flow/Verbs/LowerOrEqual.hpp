c694103d61c86f8e6da2e247bbc8cf2bee54f69b
Dimo Markov <dimomarkov@gmail.com>
2024-09-25 10:42:39 +0300

    Added missing includes for new verbs

---
+++ include/Flow/Verbs/LowerOrEqual.hpp
@@ -0,0 +1,9 @@
+///                                                                           
+/// Langulus::Flow                                                            
+/// Copyright (c) 2017 Dimo Markov <team@langulus.com>                        
+/// Part of the Langulus framework, see https://langulus.com                  
+///                                                                           
+/// SPDX-License-Identifier: GPL-3.0-or-later                                 
+///                                                                           
+#pragma once
+#include "../../../source/verbs/LowerOrEqual.inl"
